4a74de060780143d41d1ae08ec9f9e130f66e408
INABA Hitoshi <ina@cpan.org>
2011-12-23 11:19:07 +0000

    Import of INA/Char-Big5HKSCS-0.79 from CPAN.
    gitpan-cpan-distribution: Char-Big5HKSCS
    gitpan-cpan-version:      0.79
    gitpan-cpan-path:         INA/Char/Big5HKSCS/Char-Big5HKSCS-0.79.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Big5HKSCS.pm
+++ Big5HKSCS.pm
@@ -18,7 +18,7 @@ use Ebig5hkscs;
 
 BEGIN { eval q{ use vars qw($VERSION) } }
 
-$VERSION = sprintf '%d.%02d', q$Revision: 0.78 $ =~ m/(\d+)/oxmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.79 $ =~ m/(\d+)/oxmsg;
 
 # poor Symbol.pm - substitute of real Symbol.pm
 BEGIN {
@@ -5161,7 +5161,7 @@ incompatible upgrade part to traditional Perl should be rewound.
 You need write 'use Big5HKSCS;' in your script.
 
   ---------------------------------
-  Before      After
+  Before      You do
   ---------------------------------
   (nothing)   use Big5HKSCS;
   ---------------------------------
@@ -5248,10 +5248,12 @@ Also POSIX-style character classes.
   [:digit:]     [\x30-\x39]
   [:graph:]     [\x21-\x7F]
   [:lower:]     [\x61-\x7A]
+                [\x41-\x5A\x61-\x7A]     (/i modifier)
   [:print:]     [\x20-\x7F]
   [:punct:]     [\x21-\x2F\x3A-\x3F\x40\x5B-\x5F\x60\x7B-\x7E]
   [:space:]     [\x09\x0A\x0B\x0C\x0D\x20]
   [:upper:]     [\x41-\x5A]
+                [\x41-\x5A\x61-\x7A]     (/i modifier)
   [:word:]      [\x30-\x39\x41-\x5A\x5F\x61-\x7A]
   [:xdigit:]    [\x30-\x39\x41-\x46\x61-\x66]
   [:^alnum:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x30-\x39\x41-\x5A\x61-\x7A])
@@ -5262,10 +5264,12 @@ Also POSIX-style character classes.
   [:^digit:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x30-\x39])
   [:^graph:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x21-\x7F])
   [:^lower:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x61-\x7A])
+                (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE])           (/i modifier)
   [:^print:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x20-\x7F])
   [:^punct:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x21-\x2F\x3A-\x3F\x40\x5B-\x5F\x60\x7B-\x7E])
   [:^space:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x09\x0A\x0B\x0C\x0D\x20])
   [:^upper:]    (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x41-\x5A])
+                (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE])           (/i modifier)
   [:^word:]     (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x30-\x39\x41-\x5A\x5F\x61-\x7A])
   [:^xdigit:]   (?:[\x81-\xFE][\x00-\xFF]|[^\x81-\xFE\x30-\x39\x41-\x46\x61-\x66])
   ---------------------------------------------------------------------------
@@ -5328,7 +5332,7 @@ functions.
 
 =head1 Un-Escaping bytes::* Functions (Big5HKSCS.pm provides)
 
-Big5HKSCS.pm remove 'bytes::' at head of function name.
+Big5HKSCS.pm removes 'bytes::' at head of function name.
 
   ------------------------------------
   Before           After
@@ -5343,7 +5347,7 @@ Big5HKSCS.pm remove 'bytes::' at head of function name.
 
 =head1 Un-Escaping \ Of \N, \p, \P and \X (Big5HKSCS.pm provides)
 
-Big5HKSCS.pm remove '\' at head of alphanumeric regexp metasymbols \N, \p, \P
+Big5HKSCS.pm removes '\' at head of alphanumeric regexp metasymbols \N, \p, \P
 and \X. By this method, you can avoid the trap of the abstraction.
 
   ------------------------------------
@@ -5365,37 +5369,37 @@ and \X. By this method, you can avoid the trap of the abstraction.
 
 Insert 'Ebig5hkscs::' instead of '-' of operator.
 
-  ---------------------------------
-  Before      After
-  ---------------------------------
-  -r          Ebig5hkscs::r
-  -w          Ebig5hkscs::w
-  -x          Ebig5hkscs::x
-  -o          Ebig5hkscs::o
-  -R          Ebig5hkscs::R
-  -W          Ebig5hkscs::W
-  -X          Ebig5hkscs::X
-  -O          Ebig5hkscs::O
-  -e          Ebig5hkscs::e
-  -z          Ebig5hkscs::z
-  -f          Ebig5hkscs::f
-  -d          Ebig5hkscs::d
-  -l          Ebig5hkscs::l
-  -p          Ebig5hkscs::p
-  -S          Ebig5hkscs::S
-  -b          Ebig5hkscs::b
-  -c          Ebig5hkscs::c
-  -t          -t
-  -u          Ebig5hkscs::u
-  -g          Ebig5hkscs::g
-  -k          Ebig5hkscs::k
-  -T          Ebig5hkscs::T
-  -B          Ebig5hkscs::B
-  -s          Ebig5hkscs::s
-  -M          Ebig5hkscs::M
-  -A          Ebig5hkscs::A
-  -C          Ebig5hkscs::C
-  ---------------------------------
+  --------------------------------------------------------------------------
+  Before   After      Meaning
+  --------------------------------------------------------------------------
+  -r       Ebig5hkscs::r   File is readable by effective uid/gid
+  -w       Ebig5hkscs::w   File is writable by effective uid/gid
+  -x       Ebig5hkscs::x   File is executable by effective uid/gid
+  -o       Ebig5hkscs::o   File is owned by effective uid
+  -R       Ebig5hkscs::R   File is readable by real uid/gid
+  -W       Ebig5hkscs::W   File is writable by real uid/gid
+  -X       Ebig5hkscs::X   File is executable by real uid/gid
+  -O       Ebig5hkscs::O   File is owned by real uid
+  -e       Ebig5hkscs::e   File exists
+  -z       Ebig5hkscs::z   File has zero size
+  -f       Ebig5hkscs::f   File is a plain file
+  -d       Ebig5hkscs::d   File is a directory
+  -l       Ebig5hkscs::l   File is a symbolic link
+  -p       Ebig5hkscs::p   File is a named pipe (FIFO)
+  -S       Ebig5hkscs::S   File is a socket
+  -b       Ebig5hkscs::b   File is a block special file
+  -c       Ebig5hkscs::c   File is a character special file
+  -t       -t         Filehandle is opened to a tty
+  -u       Ebig5hkscs::u   File has setuid bit set
+  -g       Ebig5hkscs::g   File has setgid bit set
+  -k       Ebig5hkscs::k   File has sticky bit set
+  -T       Ebig5hkscs::T   File is a text file
+  -B       Ebig5hkscs::B   File is a binary file (opposite of -T)
+  -s       Ebig5hkscs::s   File has nonzero size (returns size in bytes)
+  -M       Ebig5hkscs::M   Age of file (at startup) in days since modification
+  -A       Ebig5hkscs::A   Age of file (at startup) in days since last access
+  -C       Ebig5hkscs::C   Age of file (at startup) in days since inode change
+  --------------------------------------------------------------------------
 
 As of Perl 5.00503, as a form of purely syntactic sugar, you can stack file
 test operators, in a way that -w -x $file is equivalent to -x $file && -w _ .
@@ -5422,16 +5426,38 @@ Back to and see 'Escaping Your Script'. Enjoy hacking!!
 You need write 'Big5HKSCS::' at head of function name when you want character
 oriented function. See 'Character Oriented Functions'.
 
-  ---------------------------------
-  Before      After
-  ---------------------------------
-  ord         Big5HKSCS::ord
-  reverse     Big5HKSCS::reverse
-  length      Big5HKSCS::length
-  substr      Big5HKSCS::substr
-  index       Big5HKSCS::index
-  rindex      Big5HKSCS::rindex
-  ---------------------------------
+  --------------------------------------------------------
+  Function   Character Oriented   Description
+  --------------------------------------------------------
+  ord        Big5HKSCS::ord
+  reverse    Big5HKSCS::reverse
+  length     Big5HKSCS::length
+  substr     Big5HKSCS::substr
+  index      Big5HKSCS::index          See 'About Indexes'
+  rindex     Big5HKSCS::rindex         See 'About Rindexes'
+  --------------------------------------------------------
+
+  About Indexes
+  -------------------------------------------------------------------------
+  Function       Works as    Returns as   Description
+  -------------------------------------------------------------------------
+  index          Character   Byte         JPerl semantics (most useful)
+  (same as Ebig5hkscs::index)
+  Big5HKSCS::index    Character   Character    Character-oriented semantics
+  CORE::index    Byte        Byte         Byte-oriented semantics
+  (nothing)      Byte        Character    (most useless)
+  -------------------------------------------------------------------------
+
+  About Rindexes
+  -------------------------------------------------------------------------
+  Function       Works as    Returns as   Description
+  -------------------------------------------------------------------------
+  rindex         Character   Byte         JPerl semantics (most useful)
+  (same as Ebig5hkscs::rindex)
+  Big5HKSCS::rindex   Character   Character    Character-oriented semantics
+  CORE::rindex   Byte        Byte         Byte-oriented semantics
+  (nothing)      Byte        Character    (most useless)
+  -------------------------------------------------------------------------
 
 =head1 Character Oriented Functions
 
@@ -5970,7 +5996,7 @@ programming environment like at that time.
 
  Programming Perl, Second Edition
  By Larry Wall, Tom Christiansen, Randal L. Schwartz
- January 1900 (really so?)
+ October 1996
  Pages: 670
  ISBN 10: 1-56592-149-6 | ISBN 13: 9781565921498
  http://shop.oreilly.com/product/9781565921498.do
@@ -5981,6 +6007,16 @@ programming environment like at that time.
  Pages: 1104
  ISBN 10: 0-596-00027-8 | ISBN 13: 9780596000271
  http://shop.oreilly.com/product/9780596000271.do
+
+ Programming Perl, 4th Edition
+ By: Tom Christiansen, brian d foy, Larry Wall, Jon Orwant
+ Publisher: O'Reilly Media
+ Formats: Print, Ebook, Safari Books Online
+ Released: February 2012
+ Pages: 1054
+ Print ISBN: 978-0-596-00492-7 | ISBN 10: 0-596-00492-3
+ Ebook ISBN: 978-1-4493-9890-3 | ISBN 10: 1-4493-9890-1
+ http://shop.oreilly.com/product/9780596004927.do
 
  Perl Cookbook, Second Edition
  By Tom Christiansen, Nathan Torkington
